643874b044c8c59407451520578a05087d8f8deb
farhancdr <pyfrhn@gmail.com>
2022-01-10 09:29:20 +0600

    chore: :seedling: update seeder to accept admin email and password

--- seeder.js
+++ seeder.js
@@ -1,7 +1,17 @@
 const path = require('path');
 const async = require('async');
+const { argv } = require('process');
+
+const adminEmail = argv[2];
+const adminPassword = argv[3];
 
 async function init() {
+    if (!adminEmail || !adminPassword) {
+        console.log('\x1b[31m', 'WARN:', '\x1b[0m', 'Please provide admin email and password');
+        console.log('\x1b[34m', 'i.e: ', '\x1b[0m', 'yarn seed "admin@gmail.com" "password"');
+        return;
+    }
+
     const config = require(path.join(process.cwd(), 'src/config/config'));
     await config.initEnvironmentVariables();
 
@@ -19,10 +29,10 @@ async function init() {
 
     function userSeeder(callback) {
         User.findOrCreate({
-            where: { email: 'habiburrahman3089@gmail.com' }, defaults: {
+            where: { email: adminEmail }, defaults: {
                 first_name: 'System',
                 last_name: 'Admin',
-                password: 'P@ssword123'
+                password: adminPassword,
             }
         }).then(function () {
             callback();
@@ -30,7 +40,7 @@ async function init() {
     }
 
     function userProfileSeeder(callback) {
-        User.findOne({ where: { email: 'habiburrahman3089@gmail.com' } }).then(admin => {
+        User.findOne({ where: { email: adminEmail } }).then(admin => {
             const userProfiles = [
                 { title: "System Admin", description: "This is the default profile for System Admin", created_by: admin.id, updated_by: admin.id },
                 { title: "Site Admin", description: "This is the default profile for Site Admin", created_by: admin.id, updated_by: admin.id },
@@ -46,14 +56,14 @@ async function init() {
                 });
             });
         });
-    }    
+    }
 
     function userUpdateSeeder(callback) {
         User.findOne({
-            where: { email: 'habiburrahman3089@gmail.com' }
+            where: { email: adminEmail }
         }).then(admin => {
             UserProfile.findOne({ where: { title: 'System Admin' } }).then(sysAdminProfile => {
-                admin.update({ profile_id: sysAdminProfile.id }).then(function() {
+                admin.update({ profile_id: sysAdminProfile.id }).then(function () {
                     callback();
                 });
             });
@@ -66,7 +76,7 @@ async function init() {
         userUpdateSeeder
     ], function (err) {
         if (err) console.error(err);
-        else console.info('DB seed completed!');
+        else console.log('\x1b[32m', 'DB seed completed!', '\x1b[0m');
         process.exit();
     });
 }
