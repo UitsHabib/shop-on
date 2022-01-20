fe5afec29c4055d0efedec9cbcc012f93f20cba5
JisanFahim <JisanFahim@users.noreply.github.com>
2022-01-20 11:53:09 +0600

    Update profile.schema

--- src/modules/platform/profile/profile.schema.js
+++ src/modules/platform/profile/profile.schema.js
@@ -1,15 +1,15 @@
-const { object, string, array } = require("yup");
+const { object, string } = require("yup");
 
-const types = ['custom', 'standard']
+const types = ['custom', 'standard'];
 
 const profileCreateSchema = object().shape({
     title: string()
         .min(3, "Profile title must be at least 3 characters.")
         .max(50, "Profile title must be at most 255 characters long.")
         .required("Profile title is required."),
-    type: array(
-        string().oneOf(Object.values(types)).required()
-    )
+    type: string()
+        .oneOf(Object.values(types))
+        .required('Type is a required.')
         .ensure(),
     description: string().max(500, "Description must be at most 500 characters long.")
         .required("Description is required.")
@@ -19,7 +19,8 @@ const profileUpdateSchema = object().shape({
     title: string()
         .min(3, "Profile title must be at least 3 characters.")
         .max(50, "Profile title must be at most 255 characters long."),
-    type: array(string().oneOf(Object.values(types)))
+    type: string()
+        .oneOf(Object.values(types))
         .ensure(),
     description: string().max(500, "Description must be at most 500 characters long.")
 });
