163393fb742cfc7185edb5c74289bbc6853b4ad8
Mehedi Hasan <mehedi.hasan4846@gmail.com>
2022-01-24 00:21:25 +0600

    Refactor authorization.middleware file

--- src/modules/core/authorization/authorization.middlewares.js
+++ src/modules/core/authorization/authorization.middlewares.js
@@ -1,17 +1,18 @@
 const path = require("path");
-const {getUserWithPermissionRelations} = require(path.join(process.cwd(), "src/modules/platform/user/permission/permission.js"));
+const { getUserWithPermissionRelations } = require(path.join(process.cwd(), "src/modules/platform/service/permission"));
 
-async function getUserWithProfiles(id){ 
-    const userWithPermissions = await getUserWithPermissionRelations({id});
+async function getUserWithProfiles(id) {
+    const userWithPermissions = await getUserWithPermissionRelations({ id });
     return userWithPermissions;
 }
 
 async function getProfilePermissions(profile) {
     const services = [];
+
     if (profile) {
-        for (const profilePermission of profile.profile_permission) {
-            let permission = profilePermission.permission;
-            for (const permission_service of permission.permission_service) {
+        for (const profilePermission of profile.profile_permissions) {
+            const permission = profilePermission.permission;
+            for (const permission_service of permission.permission_services) {
                 services.push(permission_service.service);
             }
         }
@@ -20,14 +21,14 @@ async function getProfilePermissions(profile) {
     }
 }
 
-async function getRolePermissions(userRole) {
-    if (!userRole) return [];
+async function getRolePermissions(role) {
+    if (!role) return [];
 
     const services = [];
 
-    for (const rolePermission of userRole.role_permission) {
-        let permission = rolePermission.permission;
-        for (const permission_service of permission.permission_service) {
+    for (const rolePermission of role.role_permissions) {
+        const permission = rolePermission.permission;
+        for (const permission_service of permission.permission_services) {
             services.push(permission_service.service);
         }
     }
@@ -36,27 +37,24 @@ async function getRolePermissions(userRole) {
 }
 
 async function isPermitted(userServices, allowedServices) {
-    if(userServices.some( userService => allowedServices.includes(userService))){
+    if (userServices.some(userService => allowedServices.includes(userService))) {
         return true;
     }
     return false;
 }
 
-
-
-const ServiceGuard = (allowedServices) =>{
-    return async function(req, res, next){
+const ServiceGuard = (allowedServices) => {
+    return async function (req, res, next) {
         const user = await getUserWithProfiles(req.user.id);
         const profileServices = await getProfilePermissions(user.profile);
         const roleServices = await getRolePermissions(user.role);
-        const userServices  = profileServices.concat(roleServices);
+        const userServices = profileServices.concat(roleServices);
 
-        if(!isPermitted(userServices, allowedServices)){
+        if (!isPermitted(userServices, allowedServices)) {
             return res.status(403).send("Forbidden. You are not authorized.");
-        } 
-
+        }
         next();
-    }    
+    }
 }
 
 module.exports.ServiceGuard = ServiceGuard;
