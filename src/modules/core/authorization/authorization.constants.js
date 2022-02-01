ac479f3e5a74e931f69d14de51300db421c57c96
JisanFahim <JisanFahim@users.noreply.github.com>
2022-02-02 00:33:05 +0600

    Role manage service added.

--- src/modules/core/authorization/authorization.constants.js
+++ src/modules/core/authorization/authorization.constants.js
@@ -1,10 +1,11 @@
 const Services = {
     PLATFORM: "platform-management",
     MANAGE_USER: "manage-user",
-    MANAGE_PROFILE: "manage-profile",
+    MANAGE_PROFILE: "manage-profiles",
     MANAGE_SHOP: "manage-shop",
     MANAGE_CLIENT: "manage-client",
-    MANAGE_PERMISSION: "manage-permission", 
+    MANAGE_PERMISSION: "manage-permission",
+    MANAGE_ROLE: "manage-roles",
 };
 
-module.exports = {Services};
+module.exports = { Services };
